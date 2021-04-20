6201a89ebcf50a34d0a6fb23b65abe3fbdd5322a
= <=>
2021-04-19 20:01:03 -0500

    added create_dataset

--- translator.py
+++ translator.py
@@ -28,4 +28,17 @@ en_sentence = u"Hello!"
 print(preprocess_sentence(ko_sentence))
 print(preprocess_sentence(en_sentence))
 
+def create_dataset(path, num_examples):
+    lines = io.open(path, encoding="UTF-8").read().strip().split("\n")
+
+    word_pairs = [[preprocess_sentence(w) for w in line.split('\t')]
+    for line in lines[:num_examples]]
+
+    return zip(*word_pairs)
+
+en,ko = create_dataset(corpus_path, None)
+
+print(en[-1])
+print(ko[-1])
+
 exit()
